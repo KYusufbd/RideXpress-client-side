f3021486014f8f4a0a4caca338744957c008f287
Kazi Yusuf <yfaka001@gmail.com>
2025-03-20 13:14:33 +0600

    Added login and registration page. Still working on it

--- src/firebase/AuthProvider.jsx
+++ src/firebase/AuthProvider.jsx
@@ -1,14 +1,82 @@
 import AuthContext from "../contexts/AuthContext";
+import { signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
-const AuthProvider = ({children}) => {
+import auth from "./firebaseAuth";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-    const authInfo = {
-        
-    }
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-    return (
-        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-    );
+  // Google Auth Provider
+  const provider = new GoogleAuthProvider();
+
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        setUser(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.log(errorCode, errorMessage, email, credential)
+      });
+  };
+
+  // Create an account with email an password
+  const register = (name, email, password, imageURL) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        // Signed up
+        // Add image URL
+        updateProfile(auth.currentUser, {
+          displayName: name,
+          photoURL: imageURL,
+        })
+          .then(() => {
+            // Profile updated!
+            // Registration is successfull!
+          })
+          .catch((error) => {
+            // An error occurred
+            console.log(error);
+          });
+        // Update curren user
+        const newUser = userCredential.user;
+        setUser(newUser);
+        const userObj = { name, email };
+        const response = await postUser(userObj);
+        console.log(response);
+        toast("Registration is successful!");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  const authInfo = {
+    user,
+    loginWithGoogle,
+    register
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,183 @@
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router";
+import AuthContext from "../contexts/AuthContext";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { isStrongPassword } from "validator";
+import { toast } from "react-toastify";
+
 const Login = () => {
-    return (
-        <div>
-            <h1>Log In Or Register</h1>
+  const [warning, setWarning] = useState(false);
+  const { loginWithGoogle, register, signIn, user } = useContext(AuthContext);
+
+  // Handle Login function
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    signIn(email, password);
+  };
+
+  // Handle Register function
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const imageURL = e.target.imageURL.value;
+    if (isPasswordValid(password)) {
+      register(name, email, password, imageURL);
+      e.target.reset();
+    } else {
+      toast("Invalid passowrd!");
+      setWarning(true);
+    }
+  };
+
+  // Function for password validation:
+  const isPasswordValid = (pass) => {
+    return isStrongPassword(pass, {
+      minLength: 6,
+      minNumbers: 0,
+      minSymbols: 0,
+      minLowercase: 1,
+      minUppercase: 1,
+    });
+  };
+
+  // Navigating logged in users to home page.
+  const navigate = useNavigate();
+  useEffect(() => {
+    user && navigate("/");
+  }, [navigate, user]);
+
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row max-w-6xl">
+          <div className="text-center lg:text-left px-4 md:px-8 lg:px-12">
+            <h1 className="text-3xl lg:text-5xl md:text-4xl font-bold">
+              Login now!
+            </h1>
+            <p className="py-6 text-lg opacity-80 max-w-160 font-medium">
+              Welcome back, gamer! Log in to explore, review, and share your
+              thoughts on your favorite games.
+            </p>
+          </div>
+          <Tabs>
+            <TabList>
+              <Tab>Login</Tab>
+              <Tab>Register</Tab>
+            </TabList>
+            <TabPanel>
+              <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                  <form onSubmit={handleLogin} className="fieldset">
+                    <label className="fieldset-label">Email</label>
+                    <input
+                      required
+                      name="email"
+                      type="email"
+                      className="input"
+                      placeholder="Email"
+                    />
+                    <label className="fieldset-label">Password</label>
+                    <input
+                      required
+                      name="password"
+                      type="password"
+                      className="input"
+                      placeholder="Password"
+                    />
+                    <div>
+                      <a className="link link-hover">Forgot password?</a>
+                    </div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                  </form>
+                  <p>
+                    Don&apos;t have an account? Register{" "}
+                    <Link className="hover:link" to="/register">
+                      here
+                    </Link>
+                    .
+                  </p>
+                  <p className="text-center -mb-4">Or</p>
+                  <button
+                    className="btn btn-primary mt-4"
+                    onClick={loginWithGoogle}
+                  >
+                    Continue With Google
+                  </button>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                  <form onSubmit={handleRegister} className="fieldset">
+                    <label className="fieldset-label">Name</label>
+                    <input
+                      name="name"
+                      required
+                      type="text"
+                      className="input"
+                      placeholder="Name"
+                    />
+                    <label className="fieldset-label">Email</label>
+                    <input
+                      name="email"
+                      required
+                      type="email"
+                      className="input"
+                      placeholder="Email"
+                    />
+                    <label className="fieldset-label">
+                      Photo URL (Optional)
+                    </label>
+                    <input
+                      name="imageURL"
+                      type="url"
+                      className="input"
+                      placeholder="Photo URL"
+                    />
+                    <label className="fieldset-label">Password</label>
+                    <input
+                      name="password"
+                      required
+                      type="password"
+                      className="input"
+                      placeholder="Password"
+                    />
+                    {warning && (
+                      <p className="text-warning">
+                        Password must be at least 6 characters long and include
+                        at least one uppercase letter and one lowercase letter.
+                      </p>
+                    )}
+                    <button className="btn btn-neutral mt-4">Register</button>
+                  </form>
+                  <p>
+                    Already have an account? Login{" "}
+                    <Link className="hover:link" to="/login">
+                      here
+                    </Link>
+                    .
+                  </p>
+                  <p className="text-center -mb-4">Or</p>
+                  <button
+                    className="btn btn-primary mt-4"
+                    onClick={loginWithGoogle}
+                  >
+                    Continue With Google
+                  </button>
+                </div>
+              </div>
+            </TabPanel>
+          </Tabs>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Login;
