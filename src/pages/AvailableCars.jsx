768ba82d7f11c8efbb24eb65971b47b5f81953d8
Kazi Yusuf <yfaka001@gmail.com>
2025-04-02 20:20:49 +0600

    CarDetails page structure is complete. Now time to develop booking system.

--- src/pages/AvailableCars.jsx
+++ src/pages/AvailableCars.jsx
@@ -85,6 +85,7 @@ const AvailableCars = () => {
             </button>
           </div>
         </div>
+        
         {/* Cars list */}
         {!listView && (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-3">
--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import DataContext from "../contexts/DataContext";
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 const CarDetails = () => {
   const [car, setCar] = useState();
@@ -22,37 +23,66 @@ const CarDetails = () => {
       });
   }, [carId, setLoading]);
 
+  // Handle booking
+  const handleBooking = () => {
+    // Add booking logic here
+    console.log("Booking car:", car?.model);
+  };
+
   return (
     <div>
-      <div>
-        <img src={car?.imageUrl} alt={car?.model} className="w-full" />
+      {/* Top bar */}
+      <div className="flex flex-row flex-wrap gap-5 justify-start p-3 bg-base-100">
+        <Link to="/cars" className="btn btn-ghost w-fit p-0 my-auto">
+          <IoIosArrowRoundBack size={"2rem"} />
+        </Link>
+        <h1 className="text-3xl font-bold text-secondary text-center">
+          Car Details
+        </h1>
       </div>
-      <h4 className="text-2xl font-medium">{car?.model}</h4>
+      {/* Car details card */}
+      <div className="flex flex-col items-center justify-start py-6 w-full bg-base-300 min-h-screen my-0">
+        <div className="card bg-base-100 w-120 max-w-full shadow-sm">
+          <figure className="w-full aspect-video">
+            <img src={car?.imageUrl} alt={car?.model} className="w-full" />
+          </figure>
+          <div className="card-body text-primary">
+            <div className="flex flex-row flex-wrap justify-between w-full gap-2 items-center">
+              <h4 className="card-title text-2xl font-bold">{car?.model}</h4>
+              <div className="badge badge-secondary">
+                {car?.availability ? "Avilable" : "Not Available"}
+              </div>
+            </div>
+            <p className="font-medium opacity-70">{car?.description}</p>
+            <p className="opacity-70">Location: {car?.location}</p>
+            <div className="flex flex-row flex-wrap justify-between items-center w-full opacity-90">
+              <p className="font-medium text-lg">
+                Price Per Day: {car?.dailyRentalPrice}/- Taka
+              </p>
+              <p className="text-end">Booking Count: {car?.bookingCount}</p>
+            </div>
+            <div className="w-full border-1 bg-base-200 px-2 py-1 mt-4 rounded-sm">
+              <div className="w-full bg-base-300 text-primary">
+                <h5 className="font-medium opacity-90">Features:</h5>
+              </div>
+              <ul className="text-sm italic pl-1 opacity-70">
+                {car?.features.map((feature) => {
+                  return (
+                    <li className="mt-1" key={car?.features.indexOf(feature)}>{feature}</li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="card-actions">
+              <button onClick={handleBooking} className={`btn btn-primary ${car?.availability ? '' : 'btn-disabled'} w-full mt-4`}>
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default CarDetails;
-
-console.log("Remove these lines when the page is complete");
-
-/* 
-{
-    "_id": "67e1258bcab652051e3ccfa2",
-    "model": "Chevrolet Camaro 2020",
-    "dailyRentalPrice": 16000,
-    "availability": true,
-    "vehicleRegistrationNumber": "CAM-2345",
-    "features": [
-        "V8 Engine",
-        "Sport Mode",
-        "Convertible"
-    ],
-    "description": "A powerful muscle car with an aggressive design and thrilling performance.",
-    "bookingCount": 9,
-    "imageUrl": "https://hips.hearstapps.com/hmg-prod/images/2020-chevrolet-camaross-001-1563903153.jpg?crop=0.428xw:0.544xh;0.220xw,0.221xh&resize=768:*",
-    "location": "Dhaka, Bangladesh",
-    "ownerId": "67e05d2aea3b6eb5b1da6074",
-    "dateAdded": "2025-03-03T12:30:00Z"
-}
-*/
