1d7535abe8facf548682b3b473aec597fff2ffb5
Kazi Yusuf <yfaka001@gmail.com>
2025-04-03 11:57:29 +0600

    Add car page structure is complete. Now time to add funtionality

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -1,4 +1,19 @@
 const AddCar = () => {
+  const addCar = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {
+      model: formData.get("model"),
+      dailyRentalPrice: formData.get("dailyRentalPrice"),
+      availability: formData.get("availability") === "true",
+      vehicleRegistrationNumber: formData.get("vehicleRegistrationNumber"),
+      features: formData.get("features"),
+      description: formData.get("description"),
+      imageUrl: formData.get("imageUrl"),
+      location: formData.get("location"),
+    };
+    console.log(data); // For debugging
+  }
   return (
     <div>
       {/* Top bar */}
@@ -9,52 +24,20 @@ const AddCar = () => {
       </div>
       {/* Add car form */}
       <div className="flex flex-col items-center justify-start py-6 w-full bg-base-300 min-h-screen my-0">
-        <div className="card bg-base-100 w-120 max-w-full shadow-sm">
-          <form className="card-body text-primary">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Car Model</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter car model"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Description</span>
-              </label>
-              <textarea
-                placeholder="Enter car description"
-                className="textarea textarea-bordered"
-              ></textarea>
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Location</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter car location"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Price Per Day</span>
-              </label>
-              <input
-                type="number"
-                placeholder="Enter price per day"
-                className="input input-bordered"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary mt-4">
-              Add Car
-            </button>
-          </form>
-        </div>
+        <form onSubmit={addCar} className="card bg-base-100 flex flex-col gap-3 w-120 max-w-full shadow-sm p-6">
+          <input type="text" required name="model" placeholder="Model" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <input type="text" required name="dailyRentalPrice" placeholder="Daily Rental Price (Taka)" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <select name="availability" defaultValue={true} className="bg-base-200 px-3 py-2 rounded-sm" >
+            <option value={true}>Available</option>
+            <option value={false}>Not Available</option>
+          </select>
+          <input type="text" required name="vehicleRegistrationNumber" placeholder="Vehicle Registration Number" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <input type="text" required name="features" placeholder="Features (e.g. Wireless CarPlay, Lane Assist, Sunroof)" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <input type="text" required name="description" placeholder="Description" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <input type="text" required name="imageUrl" placeholder="Image URL" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <input type="text" required name="location" placeholder="Location (e.g. Khulna, Bangladesh)" className="bg-base-200 px-3 py-2 rounded-sm" />
+          <button type="submit" className="btn btn-primary mt-4">Add Car</button>
+        </form>
       </div>
     </div>
   );
