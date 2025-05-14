4a378e63a0af6187a78d958749a80617d9912394
Kazi Yusuf <yfaka001@gmail.com>
2025-05-14 17:13:29 +0600

    Working to add modify date function.

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,15 +1,23 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import DataContext from "../contexts/DataContext";
 import axios from "axios";
 import { Link } from "react-router";
 import AuthContext from "../contexts/AuthContext";
+import { DateTime } from "luxon";
+import swal from "sweetalert";
+import DatePicker from "react-datepicker";
 
 const MyBookings = () => {
   const { myBookings, setMyBookings, loading, setLoading } =
     useContext(DataContext);
   const { user } = useContext(AuthContext);
+  const [changeToReload, setChangeToReload] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(startDate);
+  const [bookedDates, setBookedDates] = useState([]);
+  const [bookingToBeModified, setBookingToBeModified] = useState(null);
 
-  // Fetch cars data
+  // Fetch booking data
   useEffect(() => {
     setLoading(true);
     axios
@@ -24,16 +32,50 @@ const MyBookings = () => {
         setLoading(false);
       });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  }, [user, changeToReload]);
+
+  // Delete booking function used in cancel booking
+  const deleteBooking = (id) => {
+    axios
+      .delete(`/bookings/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setChangeToReload(!changeToReload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // Cancel booking
   const cancelBooking = (id) => {
-    console.log("Cancel booking with id:", id);
+    swal({
+      title: "Are you sure?",
+      text: `Once cancelled you have to book car again!`,
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal("Your booking has been cancelled!", {
+          icon: "success",
+        });
+        deleteBooking(id);
+      } else {
+        swal("Your booking is safe!");
+      }
+    });
   };
 
-  // Modify booking date
+  // Modify booking date function used in handle date change function
   const modifyDate = (id) => {
     console.log("Modify booking date with id:", id);
+  };
+
+  const handleDateChange = (id) => {
+    setBookingToBeModified(myBookings.find((booking) => booking._id === id));
+    const modal = document.getElementById("my_modal_1");
+    modal.showModal();
   };
 
   return (
@@ -42,7 +84,7 @@ const MyBookings = () => {
         {/* Top bar */}
         <div className="flex flex-row flex-wrap gap-2 justify-between p-3 bg-base-100">
           <h1 className="text-3xl font-bold text-secondary text-center">
-            My Cars
+            My Bookings
           </h1>
         </div>
 
@@ -65,7 +107,7 @@ const MyBookings = () => {
                 return (
                   <tr key={myBookings.indexOf(booking)}>
                     <td>
-                      <div className="w-42 aspect-video overflow-hidden rounded-xl flex items-center justify-center bg-base-100 shadow-2xl">
+                      <div className="w-20 aspect-video overflow-hidden rounded-xl flex items-center justify-center bg-base-100 shadow-2xl">
                         <img
                           src={booking.car.imageUrl}
                           alt={booking.car.model}
@@ -80,7 +122,9 @@ const MyBookings = () => {
                     </td>
                     <td>
                       <p className="opacity-80 text-base font-medium text-secondary">
-                        {booking.createdAt}
+                        {DateTime.fromISO(booking.createdAt).toFormat(
+                          "dd-LL-yyyy t",
+                        )}
                       </p>
                     </td>
                     <td>
@@ -90,7 +134,8 @@ const MyBookings = () => {
                     </td>
                     <td>
                       <p className="opacity-80 text-base font-medium text-primary">
-                        {booking.status}
+                        {booking.status.charAt(0).toUpperCase() +
+                          booking.status.slice(1)}
                       </p>
                     </td>
                     {/* Action buttons */}
@@ -98,13 +143,13 @@ const MyBookings = () => {
                       <div className="flex flex-row flex-wrap gap-2">
                         <button
                           onClick={() => cancelBooking(booking._id)}
-                          className="btn btn-xs btn-primary w-22"
+                          className={`btn ${booking.status === "cancelled" && "btn-disabled"} btn-warning btn-xs w-22`}
                         >
                           Cancel
                         </button>
                         <button
-                          onClick={() => modifyDate(booking._id)}
-                          className="btn btn-xs btn-error w-22"
+                          onClick={() => handleDateChange(booking._id)}
+                          className={`btn ${booking.status === "cancelled" && "btn-disabled"} btn-xs btn-primary w-22`}
                         >
                           Modify Date
                         </button>
@@ -120,14 +165,57 @@ const MyBookings = () => {
         {myBookings.length === 0 && !loading && (
           <div className="flex flex-col items-center justify-center gap-4 h-96">
             <h1 className="text-3xl font-bold text-secondary text-center">
-              You didn't add any car yet!
+              You didn't book any car yet!
             </h1>
-            <Link to="/add-car" className="btn btn-primary w-max">
-              Add Your First Car
+            <Link to="/cars" className="btn btn-primary w-max">
+              Book A Car
             </Link>
           </div>
         )}
       </div>
+      {/* Modal for modifying booking dates */}
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box flex flex-col justify-between bg-base-200 text-base-content relative min-h-11/12 my-5">
+          <div className="flex flex-col w-full gap-2">
+            <h3 className="font-bold text-3xl text-primary mb-4">
+              Booking Summary
+            </h3>
+            <p className="py-4 -mb-4 text-base italic text-warning">
+              Please select dates to book the car:
+            </p>
+            <div className="flex flex-col gap-2">
+              <h6 className="text-lg font-medium">Start date:</h6>
+              <DatePicker
+                selected={bookingToBeModified?.startDate}
+                minDate={new Date()}
+                maxDate={new Date().setDate(new Date().getDate() + 10)}
+                excludeDates={bookedDates.map((date) => new Date(date))}
+                onChange={(date) => setStartDate(date)}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h6 className="text-lg font-medium">End date:</h6>
+              <DatePicker
+                selected={bookingToBeModified?.endDate}
+                onChange={(date) => setEndDate(date)}
+                minDate={startDate}
+                maxDate={new Date().setDate(new Date(startDate).getDate() + 9)}
+                excludeDates={bookedDates.map((date) => new Date(date))}
+              />
+            </div>
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn btn-circle btn-ghost absolute right-3 top-3 h-5 w-5">
+                X
+              </button>
+            </form>
+          </div>
+          <button onClick={modifyDate} className="btn btn-secondary">
+            Confirm
+          </button>
+        </div>
+      </dialog>
     </div>
   );
 };
