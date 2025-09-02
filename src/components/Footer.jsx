4986f0a568b3d075a36474877d858669d13f776a
Kazi Yusuf <yfaka001@gmail.com>
2025-09-02 23:33:33 +0600

    Footer section is now ready.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,47 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-500 text-neutral-content">
-      <h1 className="text-center text-5xl">This is footer</h1>
+    <footer className="bg-base-100 text-base-content w-full">
+      <div className="flex flex-row flex-wrap items-center justify-center md:justify-between p-3 w-240 max-w-full mx-auto gap-4">
+        <div className="flex flex-row justify-center items-center py-4 gap-4">
+          <div className="h-12">
+            <img src="/logo-wheel.png" alt="Logo" className="h-full" />
+          </div>
+          <h2 className="text-start text-primary text-2xl font-semibold">
+            RideXpress
+          </h2>
+        </div>
+        <div className="flex flex-row flex-wrap-reverse justify-center md:justify-between items-center gap-4 md:w-2/3 max-w-full">
+          <div className="py-1">
+            <h3 className="text-center text-sm">
+              © 2024 Sinjab Tech. All rights reserved
+            </h3>
+          </div>
+          <div className="flex flex-row gap-3 items-center">
+            <a href="https://www.facebook.com" target="_blank" className="h-8">
+              <img
+                src="./icons/facebook.png"
+                alt="facebook-icon"
+                className="h-full"
+              />
+            </a>
+            <a href="https://www.instagram.com" target="_blank" className="h-8">
+              <img
+                src="./icons/instagram.png"
+                alt="instagram-icon"
+                className="h-full"
+              />
+            </a>
+            <a href="https://www.twitter.com" target="_blank" className="h-8">
+              <img
+                src="./icons/twitter.png"
+                alt="twitter-icon"
+                className="h-full"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
