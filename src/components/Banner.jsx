7344b164ba9365eb2928d91684471672b788ba03
Kazi Yusuf <yfaka001@gmail.com>
2025-09-07 01:11:17 +0600

    Completed banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,36 @@
+import { Link } from "react-router";
+
 const Banner = () => {
-    return (
-        <div className="w-full h-96 bg-gray-200 flex items-center justify-center">
-            <h5 className="text-2xl text-center text-primary font-bold min">This is banner section!</h5>
-        </div>
-    );
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <div className="h-full w-full flex flex-col gap-12 items-center justify-center z-20 relative">
+        <h5 className="text-6xl italic text-center font-semibold text-primary-content backdrop-brightness-50 w-120 p-4 rounded-lg">
+          Hit the Road with Freedom!
+        </h5>
+        <Link
+          className="rounded-full bg-linear-to-b from-primary to-blue-500 text-primary-content px-6 py-2 text-lg hover:text-xl font-medium shadow-blue-500/80 shadow-lg hover:shadow-blue-500/50 transition-shadow duration-300"
+          to="/cars"
+        >
+          View Available Cars
+        </Link>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full z-10 overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          width="100%"
+          height="100%"
+          src="/banner-video.mp4"
+          title="Mega Mercedes-Benz Car Collection 1:18 Scale | Mercedes Dealership Diorama | Diecast Model Cars"
+          allow="autoplay;"
+          allowFullScreen
+          className="banner-video"
+        ></video>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full bg-linear-to-b from-primary to-blue-200"></div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
     <footer className="bg-base-100 text-base-content w-full">
       <div className="flex flex-row flex-wrap items-center justify-center md:justify-between p-3 w-240 max-w-full mx-auto gap-4">
-        <div className="flex flex-row justify-center items-center py-4 gap-4">
+        <Link to='/' className="flex flex-row justify-center items-center py-4 gap-4">
           <div className="h-12">
             <img src="/logo-wheel.png" alt="Logo" className="h-full" />
           </div>
           <h2 className="text-start text-primary text-2xl font-semibold">
             RideXpress
           </h2>
-        </div>
+        </Link>
         <div className="flex flex-row flex-wrap-reverse justify-center md:justify-between items-center gap-4 md:w-2/3 max-w-full">
           <div className="py-1">
             <h3 className="text-center text-sm">
@@ -19,27 +20,27 @@ const Footer = () => {
             </h3>
           </div>
           <div className="flex flex-row gap-3 items-center">
-            <a href="https://www.facebook.com" target="_blank" className="h-8">
+            <Link to="https://www.facebook.com" target="_blank" className="h-8">
               <img
                 src="./icons/facebook.png"
                 alt="facebook-icon"
                 className="h-full"
               />
-            </a>
-            <a href="https://www.instagram.com" target="_blank" className="h-8">
+            </Link>
+            <Link to="https://www.instagram.com" target="_blank" className="h-8">
               <img
                 src="./icons/instagram.png"
                 alt="instagram-icon"
                 className="h-full"
               />
-            </a>
-            <a href="https://www.twitter.com" target="_blank" className="h-8">
+            </Link>
+            <Link to="https://www.twitter.com" target="_blank" className="h-8">
               <img
                 src="./icons/twitter.png"
                 alt="twitter-icon"
                 className="h-full"
               />
-            </a>
+            </Link>
           </div>
         </div>
       </div>
